d31756a83194b8331b141b54c29b2477eec5d03e
Gaston <gastonmaffei@gmail.com>
2020-04-22 19:09:24 -0300

    enum

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/you/OctoPrint-Fractalprinter"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ['enum']
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
