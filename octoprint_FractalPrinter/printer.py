86aa3420d6b7089ca59784e931dfa7b54a756043
Gaston <gastonmaffei@gmail.com>
2020-05-31 20:30:49 -0300

    filament change commands

--- octoprint_FractalPrinter/printer.py
+++ octoprint_FractalPrinter/printer.py
@@ -8,16 +8,16 @@ class PrinterManager:
 		if filamentData is None:
 			filamentData = {'tool': 0, 'temperature': 210}
 
-		tool_name = "tool{}".format(filamentData['tool'])
-		self.printer.set_temperature(tool_name, filamentData['temperature'])
-		self.printer.commands("M600")
+		# tool_name = "tool{}".format(filamentData['tool'])
+		# self.printer.set_temperature(tool_name, filamentData['temperature'])
+		self.printer.commands(["M109 S{}".format(filamentData['temperature']), "M600"])
 
 	def printFile(self, fileData=None, path=None):
 		if fileData:
 			if self.plugin.DBManager.getFilePath(fileData['id']):
 				self.printer.select_file(path=self.plugin.DBManager.getFilePath(fileData['id']), sd=False, printAfterSelect=True)
 			else:
-				fileData['print'] = True
+				fileData['print_after'] = True
 				self.plugin.downloadManager.enqueue(fileData)
 		elif path:
 			self.printer.select_file(path=path, sd=False, printAfterSelect=True)
